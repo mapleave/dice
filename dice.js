c029505bbf00aa4b70c27f4a777ba42fd7bf5723
mapleave <43427526+mapleave@users.noreply.github.com>
2019-01-06 00:12:21 +0800

    Add files via upload

--- dice.js
+++ dice.js
@@ -1,51 +1,111 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-function dicedraw(){var data = google.visualization.arrayToDataTable([
-['1', ],['2', ], ['3', ], ['4', ], ['5', ], ['6', ]
-]);}
-function dice(){
-var count = document.getElementById("count").value
-while (count > 0) {
-
-count = count - 1;
-}
-var target=Math.floor((Math.random() * 100) + 1);;
-    var dice_results = {1:"0",2:"0",3:"0",4:"0",5:"0",6:"0"};
-    sum_1 = 0
-    sum_2 = 0
-    sum_3 = 0
-    sum_4 = 0
-    sum_5 = 0
-    sum_6 = 0
-        dice_result = random.randint(1,6)
-        if dice_result == 1:
-            sum_1 += 1
-            dice_results[dice_result] = sum_1
-        elif dice_result == 2:
-            sum_2 += 1
-            dice_results[dice_result] = sum_2
-        elif dice_result == 3:
-            sum_3 += 1
-            dice_results[dice_result] = sum_3
-        elif dice_result == 4:
-            sum_4 += 1
-            dice_results[dice_result] = sum_4
-        elif dice_result == 5:
-            sum_5 += 1
-            dice_results[dice_result] = sum_5
-        else:
-            sum_6 += 1
-            dice_results[dice_result] = sum_6
-    for i in range(1,7):
-        print('第{}次'.format(i),dice_results[i])
-    print('出現1的次數:{:.2f}'.format(float(sum_1)/enter_number))
-    print('出現2的次數:{:.2f}'.format(float(sum_2)/enter_number))
-    print('出現3的次數:{:.2f}'.format(float(sum_3)/enter_number))
-    print('出現4的次數:{:.2f}'.format(float(sum_4)/enter_number))
-    print('出現5的次數:{:.2f}'.format(float(sum_5)/enter_number))
-    print('出現6的次數:{:.2f}'.format(float(sum_6)/enter_number))
-    
-if(document.getElementById("number").value > target){alert("猜小一些");}
-else if (document.getElementById("number").value < target){alert("猜大一些");}
-alert("出現1的機率:" + dice_results[1] + " is " + person.age + " years old.");
-}
+function getRandom(min,max){
+			return Math.floor(Math.random()*(max-min+1))+min;
+		}
+		function dice1(){
+			var answer=document.getElementById("answer").value;
+			if (parseInt(answer)==answer&&parseInt(answer)>=0){
+				var diceTimeOne=0;
+				var diceTimeTwo=0;
+				var diceTimeThree=0;
+				var diceTimeFour=0;
+				var diceTimeFive=0;
+				var diceTimeSix=0;
+				for(i=0;i<answer;i++){
+					diceTime=getRandom(1,6)
+					if (diceTime==1){
+						diceTimeOne=parseInt(diceTimeOne)+1
+					}
+					if (diceTime==2){
+						diceTimeTwo=parseInt(diceTimeTwo)+1
+					}
+					if (diceTime==3){
+						diceTimeThree=parseInt(diceTimeThree)+1
+					}
+					if (diceTime==4){
+						diceTimeFour=parseInt(diceTimeFour)+1
+					}
+					if (diceTime==5){
+						diceTimeFive=parseInt(diceTimeFive)+1
+					}
+					if (diceTime==6){
+						diceTimeSix=parseInt(diceTimeSix)+1
+					}
+					var x=i+1;
+					alert("第"+x+"次擲骰子:"+diceTime+"\n"+"目前機率: "+"\n"+"1點機率:"+(diceTimeOne/x)+"\n"+"2點機率:"+(diceTimeTwo/x)+"\n"+"3點機率:"+(diceTimeThree/x)+"\n"+"4點機率:"+(diceTimeFour/x)+"\n"+"5點機率"+(diceTimeFive/x)+"\n"+"6點機率:"+(diceTimeSix/x));
+				}
+			}
+			else{
+				alert("你他媽是文盲還是來亂的?"+"\n"+"最好是可以擲"+answer+"次骰子!");
+			}
+			if (parseInt(answer)==answer&&parseInt(answer)>=0){
+			google.charts.load('current', {'packages':['corechart']});
+			google.charts.setOnLoadCallback(drawChart);
+		}
+			function drawChart(){
+				var data = google.visualization.arrayToDataTable([
+			['點數', '出現機率'],
+			['1', (diceTimeOne/x)], ['2', (diceTimeTwo/x)], ['3', (diceTimeThree/x)],
+			['4', (diceTimeFour/x)], ['5', (diceTimeFive/x)], ['6', (diceTimeSix/x)],
+			]);
+			var options = {
+			title: '各點出現機率',
+			};
+			var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+			chart.draw(data, options);
+			}
+		}
+		function dice2(){
+			var answer=document.getElementById("answer").value;
+			if (parseInt(answer)==answer&&parseInt(answer)>=0){
+				var diceTimeOne=0;
+				var diceTimeTwo=0;
+				var diceTimeThree=0;
+				var diceTimeFour=0;
+				var diceTimeFive=0;
+				var diceTimeSix=0;
+				for(i=0;i<answer;i++){
+					diceTime=getRandom(1,6)
+					if (diceTime==1){
+						diceTimeOne=parseInt(diceTimeOne)+1
+					}
+					if (diceTime==2){
+						diceTimeTwo=parseInt(diceTimeTwo)+1
+					}
+					if (diceTime==3){
+						diceTimeThree=parseInt(diceTimeThree)+1
+					}
+					if (diceTime==4){
+						diceTimeFour=parseInt(diceTimeFour)+1
+					}
+					if (diceTime==5){
+						diceTimeFive=parseInt(diceTimeFive)+1
+					}
+					if (diceTime==6){
+						diceTimeSix=parseInt(diceTimeSix)+1
+					}
+					var x=i+1;
+				}
+			}
+			else{
+				alert("你他媽是文盲還是來亂的?"+"\n"+"最好是可以擲"+answer+"次骰子!");
+			}
+			if (parseInt(answer)==answer&&parseInt(answer)>=0){
+			alert("總共擲"+x+"次骰子"+"\n"+"各點機率: "+"\n"+"1點機率:"+(diceTimeOne/x)+"\n"+"2點機率:"+(diceTimeTwo/x)+"\n"+"3點機率:"+(diceTimeThree/x)+"\n"+"4點機率:"+(diceTimeFour/x)+"\n"+"5點機率"+(diceTimeFive/x)+"\n"+"6點機率:"+(diceTimeSix/x));
+			}
+			if (parseInt(answer)==answer&&parseInt(answer)>=0){
+			google.charts.load('current', {'packages':['corechart']});
+			google.charts.setOnLoadCallback(drawChart);
+			}
+			function drawChart(){
+				var data = google.visualization.arrayToDataTable([
+			['點數', '出現機率'],
+			['1', (diceTimeOne/x)], ['2', (diceTimeTwo/x)], ['3', (diceTimeThree/x)],
+			['4', (diceTimeFour/x)], ['5', (diceTimeFive/x)], ['6', (diceTimeSix/x)],
+			]);
+			var options = {
+			title: '各點出現機率',
+			};
+			var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+			chart.draw(data, options);
+			}
+		}
